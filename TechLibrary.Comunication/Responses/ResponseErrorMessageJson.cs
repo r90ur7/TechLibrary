31c99ac422b54127a0421941470b992ff6efb9cd
Rallenson <Rallenson90@gmail.com>
2025-02-23 03:56:15 -0300

    Adicionar arquivos de projeto.

---
+++ TechLibrary.Comunication/Responses/ResponseErrorMessageJson.cs
@@ -0,0 +1,7 @@
+﻿namespace TechLibrary.Comunication.Responses
+{
+    public class ResponseErrorMessageJson
+    {
+        public List<string> Errors { get; set; } = new List<string>();
+    }
+}
---
+++ TechLibrary.Comunication/Responses/ResponseRegisterUserJson.cs
@@ -0,0 +1,10 @@
+﻿namespace TechLibrary.Comunication.Responses
+{
+    public class ResponseRegisterUserJson
+    {
+        public string Name { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
